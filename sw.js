065f76794e3fcdcdb7882beacd945b7171a73590
cathydelormepets-source <cathydelormepets@gmail.com>
2026-03-26 11:02:25 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(n) {
-      return Promise.all(n.map(function(k) { return caches.delete(k); }));
-    }).then(function() { return self.registration.unregister(); })
-  );
-});
