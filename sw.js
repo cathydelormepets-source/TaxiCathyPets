32b522ab62604a0d46fb99342d8ee06a074f92d4
cathydelormepets-source <cathydelormepets@gmail.com>
2026-03-26 12:59:25 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(n) {
+      return Promise.all(n.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.registration.unregister(); })
+  );
+});
